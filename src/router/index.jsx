22941a7620f3b1faa6ee780eae6b5714e29e208e
Jose Angel Lopez Mondragon <angel@customleads.ai>
2025-07-29 11:58:51 -0600

    [ADD] remotes de ejemplo de todos

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,6 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from '../App';
 import NotFound from '../pages/NotFound';
 const UserAngel = lazy(() => import('user_angel/UserAngel'));
+const UserGabriel = lazy(() => import('user_gabriel/UserGabriel'));
+const UserAidalit = lazy(() => import('appAida/UserAida'));
+const UserLuis = lazy(() => import('user_luis/UserLuis'));
+const UserJordan = lazy(() => import('user_jordan/UserJordan'));
 
 const Router = () => {
   console.log('Router component rendered');
@@ -15,6 +19,26 @@ const Router = () => {
           <Route path='/angel' element={
             <Suspense fallback={<div><p>Cargando...</p></div>}>
               <UserAngel />
+            </Suspense>
+          }/>
+          <Route path='/gabriel' element={
+            <Suspense fallback={<div><p>Cargando...</p></div>}>
+              <UserGabriel />
+            </Suspense>
+          }/>
+          <Route path='/aidalit' element={
+            <Suspense fallback={<div><p>Cargando...</p></div>}>
+              <UserAidalit />
+            </Suspense>
+          }/>
+          <Route path='/luis' element={
+            <Suspense fallback={<div><p>Cargando...</p></div>}>
+              <UserLuis />
+            </Suspense>
+          }/>
+          <Route path='/jordan' element={
+            <Suspense fallback={<div><p>Cargando...</p></div>}>
+              <UserJordan />
             </Suspense>
           }/>
           <Route path='*' element={<NotFound />}/>
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,131 @@ module.exports = {
       name: 'app_shell',
       remotes: {
         app_products: 'app_products@http://localhost:3001/remoteEntry.js',
-        user_angel: 'user_angel@http://localhost:3002/remoteEntry.js',
+        user_angel: `promise new Promise(resolve => {
+            fetch("http://localhost:8080/remote.manifest.json")
+              .then(res => res.json())
+              .then(remotes => {
+                const remoteUrl = remotes.user_angel.split("@")[1];
+                const remoteGlobal = remotes.user_angel.split("@")[0];
+                const script = document.createElement("script");
+                script.src = remoteUrl
+                script.onload = () => {
+                  const proxy = {
+                    get: (request) => window[remoteGlobal].get(request),
+                    init: (arg) => {
+                      try {
+                        return window[remoteGlobal].init(arg);
+                      } catch (error) {
+                        console.log("remote already initialized");
+                      }
+                    }
+                  }
+                  resolve(proxy);
+                };
+                document.head.appendChild(script);
+              })
+          })
+        `,
+        user_gabriel: `promise new Promise(resolve => {
+            fetch("http://localhost:8080/remote.manifest.json")
+              .then(res => res.json())
+              .then(remotes => {
+                const remoteUrl = remotes.user_gabriel.split("@")[1];
+                const remoteGlobal = remotes.user_gabriel.split("@")[0];
+                const script = document.createElement("script");
+                script.src = remoteUrl
+                script.onload = () => {
+                  const proxy = {
+                    get: (request) => window[remoteGlobal].get(request),
+                    init: (arg) => {
+                      try {
+                        return window[remoteGlobal].init(arg);
+                      } catch (error) {
+                        console.log("remote already initialized");
+                      }
+                    }
+                  }
+                  resolve(proxy);
+                };
+                document.head.appendChild(script);
+              })
+          })
+        `,
+        appAida: `promise new Promise(resolve => {
+            fetch("http://localhost:8080/remote.manifest.json")
+              .then(res => res.json())
+              .then(remotes => {
+                const remoteUrl = remotes.appAida.split("@")[1];
+                const remoteGlobal = remotes.appAida.split("@")[0];
+                const script = document.createElement("script");
+                script.src = remoteUrl
+                script.onload = () => {
+                  const proxy = {
+                    get: (request) => window[remoteGlobal].get(request),
+                    init: (arg) => {
+                      try {
+                        return window[remoteGlobal].init(arg);
+                      } catch (error) {
+                        console.log("remote already initialized");
+                      }
+                    }
+                  }
+                  resolve(proxy);
+                };
+                document.head.appendChild(script);
+              })
+          })
+        `,
+        user_jordan: `promise new Promise(resolve => {
+            fetch("http://localhost:8080/remote.manifest.json")
+              .then(res => res.json())
+              .then(remotes => {
+                const remoteUrl = remotes.user_jordan.split("@")[1];
+                const remoteGlobal = remotes.user_jordan.split("@")[0];
+                const script = document.createElement("script");
+                script.src = remoteUrl
+                script.onload = () => {
+                  const proxy = {
+                    get: (request) => window[remoteGlobal].get(request),
+                    init: (arg) => {
+                      try {
+                        return window[remoteGlobal].init(arg);
+                      } catch (error) {
+                        console.log("remote already initialized");
+                      }
+                    }
+                  }
+                  resolve(proxy);
+                };
+                document.head.appendChild(script);
+              })
+          })
+        `,
+        user_luis: `promise new Promise(resolve => {
+            fetch("http://localhost:8080/remote.manifest.json")
+              .then(res => res.json())
+              .then(remotes => {
+                const remoteUrl = remotes.user_luis.split("@")[1];
+                const remoteGlobal = remotes.user_luis.split("@")[0];
+                const script = document.createElement("script");
+                script.src = remoteUrl
+                script.onload = () => {
+                  const proxy = {
+                    get: (request) => window[remoteGlobal].get(request),
+                    init: (arg) => {
+                      try {
+                        return window[remoteGlobal].init(arg);
+                      } catch (error) {
+                        console.log("remote already initialized");
+                      }
+                    }
+                  }
+                  resolve(proxy);
+                };
+                document.head.appendChild(script);
+              })
+          })
+        `,
       },
       shared: {
         react: { singleton: true, requiredVersion: '^19.1.1' },
